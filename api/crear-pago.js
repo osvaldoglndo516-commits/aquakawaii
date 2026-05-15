824cde8ca532f711f6ed236f56b7754257db8c60
osvaldoglndo516-commits <osvaldoglndo516@gmail.com>
2026-05-15 13:42:42 -0600

    Change export to module.exports in crear-pago.js

--- api/crear-pago.js
+++ api/crear-pago.js
@@ -1,6 +1,6 @@
 const Stripe = require('stripe')
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' })
   }
